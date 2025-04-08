aa389711cb29cd8c5cb1cbf0e1da3c656e880149
OlerixXx <olegzka.mprozov@yandex.ru>
2025-04-08 21:18:18 +0300

    Sprint 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,21 @@
 const cardTemplate = document.querySelector('#card-template').content;
-const placesList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
 initialCards.forEach(element => {
-  placesList.append(createCard(element.name, element.link));
+  cardsContainer.append(createCard(element, deleteCard));
 });
 
-function createCard (name, imgLink) {
+function createCard (element, deleteCard) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  cardElement.querySelector('.card__image').src = imgLink;
-  cardElement.querySelector('.card__description .card__title').textContent = name;
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__title').alt = element.name;
 
   const deleteButton = cardElement.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', evt => removeCard(evt));
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
   return cardElement;
 }
 
-function removeCard(evt) {
-  evt.target.parentElement.remove();
+function deleteCard(cardElement) {
+  cardElement.remove();
 }
